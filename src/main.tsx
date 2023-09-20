33fdbf18451057e6390aa774277eaf938fa9daf4
okanay <okanay@hotmail.com>
2023-09-20 05:03:02 +0300

    Helmet Remove

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@
 import ReactDOM from 'react-dom/client';
 import './global.css';
 import Layout from '~/pages/layout.tsx';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
-    {/*<React.StrictMode>*/}
-    <Layout />
-    {/*</React.StrictMode>*/},
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
   </>,
 );
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,8 @@
 import { Providers } from '~/providers/providers.tsx';
-import { Helmet } from 'react-helmet';
 
 export default function Layout() {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>My Title</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
       <div className={'h-screen bg-zinc-100'}>
         <div className={'mx-auto max-w-7xl'}>
           <Providers />
